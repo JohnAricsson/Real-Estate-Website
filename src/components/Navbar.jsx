d83dc3f11f8e62ddf4fa16cb5f65cf59dc9171bd
John Aricsson Majumder <johnaricsson1@gmail.com>
2025-07-20 22:10:05 +0600

    Responsive Navbar Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {assets} from '../assets/assets'
 const Navbar = () => {
+  const[showMobileMenu, setShowMobileMenu] = useState(false)
+  useEffect(()=>{
+    if(showMobileMenu){
+      document.body.style.overflow = 'hidden'
+    }
+    else{
+      document.body.style.overflow = 'auto'
+    }
+    return ()=>{
+      document.body.style.overflow = 'auto'
+    };
+  },[showMobileMenu])
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
       <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
-        <img className='w-10 h-10 md:w-20 md:h-20 ' src={assets.logo} alt=''/>
+        <img className='w-12 h-12 md:w-17 md:h-17 ' src={assets.logo} alt=''/>
         <ul className='hidden md:flex gap-7 text-white text-2xl font-medium'>
-            <a href='#Header' className='hover:underline cursor-pointer hover:text-gray-400 '>Home</a>
-            <a href='#Header' className='hover:underline cursor-pointer hover:text-gray-400'>About</a>
-            <a href='#Header' className='hover:underline cursor-pointer hover:text-gray-400'>Projects</a>
-            <a href='#Header' className='hover:underline cursor-pointer hover:text-gray-400'>Testimonials</a>   
+            <a href='#Header' className='hover:underline cursor-pointer hover:text-blue-500 '>Home</a>
+            <a href='#About' className='hover:underline cursor-pointer hover:text-blue-500'>About</a>
+            <a href='#Projects' className='hover:underline cursor-pointer hover:text-blue-500'>Projects</a>
+            <a href='#Testimonials' className='hover:underline cursor-pointer hover:text-blue-500'>Testimonials</a>   
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>
-        <img src={assets.menu_icon} className='md:hidden w-7'/>
+        <img onClick={()=>setShowMobileMenu(true)} src={assets.menu_icon} className='md:hidden w-7 cursor-pointer'/>        
+      </div>
+      {/* Menu items for mobile */}
+      <div className={`md:hidden ${showMobileMenu?'fixed w-full': 'h-0 w-0'} right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
+         <div className='flex justify-end p-6 cursor-pointer'>
+          <img onClick={()=>setShowMobileMenu(false)}src={assets.cross_icon} className='w-6'/>
+         </div>
+         <ul className='flex flex-col items-center gap-2 mt-5 mx-5 text-lg font-medium'>
+          <a onClick={()=>setShowMobileMenu(false)} href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
+          <a onClick={()=>setShowMobileMenu(false)} href="#About" className='px-4 py-2 rounded-full inline-block'>About</a>
+          <a onClick={()=>setShowMobileMenu(false)} href="#Projects" className='px-4 py-2 rounded-full inline-block'>Projects</a>
+          <a onClick={()=>setShowMobileMenu(false)} href="#Testimonials" className='px-4 py-2 rounded-full inline-block'>Testimonials</a>
+
+         </ul>
       </div>
     </div>
   )
